d387dfe5d8ed31c52731c75d27d0ef0204f4699f
Sergei Kamakov <sergei.kamakov@simbirsoft.com>
2025-09-26 17:31:32 +0300

    Add font registering

--- mobileSDK.UI/mobileSDK.UI/Common/ViewParams/Fonts.swift
+++ mobileSDK.UI/mobileSDK.UI/Common/ViewParams/Fonts.swift
@@ -19,6 +19,11 @@ protocol FontLibrary {
 }
 
 struct DefaultFontLibrary: FontLibrary {
+    
+    init() {
+        registerFonts()
+    }
+    
     let screenHeader: Font = Font.custom("Inter-Bold", size: 22)
 
     func commonRegular(size: CGFloat) -> Font {
@@ -39,3 +44,31 @@ struct DefaultFontLibrary: FontLibrary {
     }
 }
 
+
+public let fontBundle = SDKBundle.get()
+
+public func registerFonts() {
+    UIFont.registerFont(bundle: fontBundle, fontName: "Inter-Regular", fontExtension: "ttf")
+    UIFont.registerFont(bundle: fontBundle, fontName: "Inter-SemiBold", fontExtension: "ttf")
+    UIFont.registerFont(bundle: fontBundle, fontName: "Inter-Bold", fontExtension: "ttf")
+}
+
+extension UIFont {
+    static func registerFont(bundle: Bundle, fontName: String, fontExtension: String) {
+
+        guard let fontURL = bundle.url(forResource: fontName, withExtension: fontExtension) else {
+            fatalError("Couldn't find font \(fontName)")
+        }
+
+        guard let fontDataProvider = CGDataProvider(url: fontURL as CFURL) else {
+            fatalError("Couldn't load data from the font \(fontName)")
+        }
+
+        guard let font = CGFont(fontDataProvider) else {
+            fatalError("Couldn't create font from data")
+        }
+
+        var error: Unmanaged<CFError>?
+        CTFontManagerRegisterGraphicsFont(font, &error)
+    }
+}
